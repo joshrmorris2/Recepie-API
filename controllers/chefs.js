019f36249b28653ab7677a692719c2c780cdefee
joshrmorris2 <moofase1@gmail.com>
2024-02-23 21:43:23 -0800

    chefs controller created

---
+++ controllers/chefs.js
@@ -0,0 +1,101 @@
+const { response } = require('express');
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = (req, res) => {
+    mongodb
+        .getDb()
+        .db()
+        .collection('chefs')
+        .find()
+        .toArray((err, lists) => {
+            if (err) {
+                res.status(400).json({ message: err });
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+    });
+};
+
+const getSingle = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid recipe id to find a recipe.');
+    }
+    const userId = new ObjectId(req.params.id);
+    mongodb
+        .getDb()
+        .db()
+        .collection('chefs')
+        .find({ _id: userId })
+        .toArray((err, lists) => {
+            if (err) {
+                res.status(400).json({message:err});
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+    });
+};
+
+const createRecipe = async (req, res, next) => {
+    const recipe = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        cuisine: req.body.cuisine,
+        email: req.body.email,
+        phone: req.body.phone,
+        forHire: req.body.forHire,
+    };
+    const response = await mongodb.getDb().db().collection('chefs').insertOne(recipe);
+    if (response.acknowledged) {
+        res.status(201).json(response);
+    } else {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const updateRecipe = async (req, res, next) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use a valid recipe id to update a recipe.');
+    }
+    const response = await mongodb.getDb().db().collection('chefs').updateOne(
+        { _id: new ObjectId(req.params.id) }, 
+        {
+            $set: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                cuisine: req.body.cuisine,
+                email: req.body.email,
+                phone: req.body.phone,
+                forHire: req.body.forHire,
+            },
+        });
+        if(response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+};
+
+const deleteRecipe = async (req, res, next) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use a valid recipe id to delete a recipe.');
+    }
+    const result = await mongodb.getDb().db().collection('chefs').deleteOne(
+        { _id: new ObjectId(req.params.id) }, true);
+
+    if (result.deletedCount > 0 ) {
+        res.status(200).send();
+    } else {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+module.exports = {
+    getAll,
+    getSingle,
+    createRecipe,
+    updateRecipe,
+    deleteRecipe
+};

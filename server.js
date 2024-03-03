180207af57282ea84495190e3c7bf1eda40769f5
joshrmorris2 <moofase1@gmail.com>
2024-03-03 15:14:15 -0800

    isAuthenticated console.log

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const mongodb = require('./db/connect');
 const passport = require('./middleware/oauth');
 const { isAuthenticated } = require('./routes/oauth');
+console.log('isAuthenticated:', isAuthenticated);
 
 const port = process.env.PORT || 8080;
 const app = express();

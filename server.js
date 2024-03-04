3957ecbb7893bb48d16a1140892e9fe8d8656aa0
joshrmorris2 <moofase1@gmail.com>
2024-03-03 16:33:46 -0800

    imports for server.js fixed

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const mongodb = require('./db/connect');
 const passport = require('./middleware/oauth');
-const isAuthenticated = require('../middleware/oauth').isAuthenticated;
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -33,7 +32,7 @@ app
   .use(passport.initialize())
   .use(passport.session());
 
-app.use('/', isAuthenticated, require('./routes'));
+app.use('/', passport.isAuthenticated, require('./routes'));
 
 process.on('uncaughtException', (err, origin) => {
   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);

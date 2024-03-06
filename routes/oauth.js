d18dcc12aed5d68b882a2e61f93b44217c0f6d5e
joshrmorris2 <moofase1@gmail.com>
2024-03-05 22:34:24 -0800

    oauth route fixes

--- routes/oauth.js
+++ routes/oauth.js
@@ -6,16 +6,16 @@ const router = express.Router();
 // Authentication routes
 router.get('/google', (req, res, next) => {
   console.log('Entering /auth/google route');
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  passport.authenticate('google', { scope: ['profile', 'email'] })(req, res, next);
 });
 
-router.get('/google/callback', (req, res, next) => {
-  console.log('Entering /auth/google/callback route');
+router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     // Successful authentication, redirect to a success page or handle as needed
+    console.log('Entering /auth/google/callback route');
     res.redirect('/api-docs');
   }
-});
+);
 
 module.exports = router;

1589ff16cff644fe653710a1c91ab13d9bafbfbf
joshrmorris2 <moofase1@gmail.com>
2024-03-05 21:24:56 -0800

    import fixed

--- middleware/oauth.js
+++ middleware/oauth.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const credentials = './credentials';
+const credentials = require('./credentials.json');
 
 passport.use(new GoogleStrategy({
     clientID: credentials.clientID,

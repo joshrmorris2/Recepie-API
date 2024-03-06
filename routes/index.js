45a4a942f8da99c1299000107d0227ddaa499155
joshrmorris2 <moofase1@gmail.com>
2024-03-05 23:09:47 -0800

    removed redundant route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const isAuthenticated = require('../middleware/oauth').isAuthenticated;
 
 router.use('/auth', require('./oauth'));
-router.use('/auth', isAuthenticated);
 router.use('/recipes', require('./recipes'));
 router.use('/chefs', require('./chefs'));
 router.use('/', require('./swagger'));

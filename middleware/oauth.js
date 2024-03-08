791763e563213c1d3aa531af0930c138662f1489
joshrmorris2 <moofase1@gmail.com>
2024-03-07 21:53:43 -0800

    removed troubleshooting logs

--- middleware/oauth.js
+++ middleware/oauth.js
@@ -26,11 +26,9 @@ passport.deserializeUser((obj, done) => {
 // Middleware to check if the user is authenticated
 passport.isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
-    console.log('in isAuthenticated if statement')
     return next();
   }
   // Redirect to login or handle unauthorized access as needed
-  console.log('in isAuthenticated failed if statement')
   res.redirect('/auth/google');
 };
 
